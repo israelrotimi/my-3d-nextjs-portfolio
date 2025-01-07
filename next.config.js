f642c6f9917dca08b7bc82021d6c7f6dcde1bb4f
Israel Rotimi <130306892+israelrotimi@users.noreply.github.com>
2025-01-07 18:14:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ export const sentryConfig = withSentryConfig(
 
     org: "israel-rotimi",
     project: "javascript-nextjs",
-    typescript: {ignorebuilderrors: true},
+    typescript: {ignoreBuildErrors: true},
 
     // Only print logs for uploading source maps in CI
     silent: !process.env.CI,
