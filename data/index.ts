588d100b279e0958e3e3f43d45d4dd50923c412f
Israel Rotimi <130306892+israelrotimi@users.noreply.github.com>
2025-02-07 10:44:34 +0100

    Update recent projects

--- data/index.ts
+++ data/index.ts
@@ -76,7 +76,7 @@ export const navItems = [
       des: "Explore games to play in your browser.",
       img: "/game-max.png",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
-      link: "https://github.com/israelrotimi/game-max",
+      link: "https://game-max-tjf2.vercel.app",
     },
     // {
     //   id: 1,
@@ -102,14 +102,14 @@ export const navItems = [
     //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     //   link: "https://github.com/adrianhajdin/ai_saas_app",
     // },
-    // {
-    //   id: 4,
-    //   title: "Animated Apple Iphone 3D Website",
-    //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    //   img: "/p4.svg",
-    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    //   link: "https://github.com/adrianhajdin/iphone",
-    // },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://iphone-15-clone-with-react.vercel.app",
+    },
   ];
   
   export const testimonials = [
